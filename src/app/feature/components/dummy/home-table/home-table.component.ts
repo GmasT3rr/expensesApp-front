81ac1bab18157dd2bb11041df1e36cab6572a248
GmasT3rr <lucramallo14@gmail.com>
2022-11-20 19:55:51 -0300

    ADD: Sum de todos los gatos en el mes filtrado

--- src/app/feature/components/dummy/home-table/home-table.component.ts
+++ src/app/feature/components/dummy/home-table/home-table.component.ts
@@ -7,10 +7,22 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class HomeTableComponent implements OnInit {
   @Input() expensesInfo:any
+  public totalSpent:any
 
   constructor() { }
 
   ngOnInit(): void {
+    this.expenseSum()
+  }
+
+  expenseSum(){
+    const valuesArray:any = []
+    this.expensesInfo.forEach((expense:any) => {
+      valuesArray.push(expense.price)
+    });
+    let sum = valuesArray.reduce((a:any,b:any)=>a+b,0)
+    this.totalSpent = sum
+    console.log(this.totalSpent);
   }
 
 }
